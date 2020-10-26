09579a18aa36cc1dd7eee0fc96aa2f804e242b89
Luigi Raymundo <lraymundo25@gmail.com>
2020-10-26 00:32:45 -0500

    "Agregando numpy y dockers commands"

---
+++ Python Scripts/UsuariosOpmanager.py
@@ -0,0 +1,17 @@
+## This code is for create user in OpManager / MRA
+## Over API Rest  with API KEY  44a57108065a4950a4756d96d7f2099e
+
+## Remenber that with curl I can send http message as get post delete etc.
+
+import os
+import subprocess
+
+name = input("Ingrese el nombre del usuario\n")
+password = input("Ingrese la password\n")
+key=44a57108065a4950a4756d96d7f2099e
+
+def UserCreation(name,password):
+
+	subprocess.run("curl -X GET http://192.168.1.90:8060/api/json/admin/listUsers?apiKey=44a57108065a4950a4756d96d7f2099e")
+
+	
