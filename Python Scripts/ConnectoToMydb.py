d02b7da5005d1e4370b07d1c088da8a1a2aefb57
Luigi Raymundo <lraymundo25@gmail.com>
2020-10-31 12:06:31 -0500

    "Python connect to DB "

---
+++ Python Scripts/ConnectoToMydb.py
@@ -0,0 +1,25 @@
+## Conectandome a mi dB 
+
+import mysql.connector
+
+
+mydb = mysql.connector.connect(
+
+
+	host="localhost",
+	user="DevUser",
+	password="FIEEyeah!1"
+
+)
+
+
+mycursor=mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+
+	print(x)
+
+
+
