f053c3cb63e4dddeb6b6b5cbca0452091ee5d68d
Luigi Raymundo <lraymundo25@gmail.com>
2021-07-12 23:00:56 -0500

    "new file tests for scrips in python"

---
+++ YAML_JS_Docker_Python_Ansible/Requests.py
@@ -0,0 +1,28 @@
+'''
+Here we make new files with a function
+
+'''
+
+
+import requests
+import io
+import subprocess
+import sys
+
+
+
+def makefiles(formato,cantidad):
+
+	while (cantidad >= 0):
+
+		file=open(f"{formato}_{cantidad}.txt","a")
+		file.write(f"Here a test for file number {cantidad}")
+		file.close()
+
+		fileend=open(f"{formato}_{cantidad}.txt")
+		print(fileend.read())
+
+		cantidad=cantidad-1
+
+
+makefiles("Copito",10)
