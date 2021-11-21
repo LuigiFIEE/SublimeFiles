4edfdd40382ee2eae197ef8a1b5e77cf0085a30e
Luigi Raymundo <lraymundo25@gmail.com>
2021-11-21 16:46:46 -0500

    Curso JAvaScript

---
+++ Curso JavaScript/Abstrac.js
@@ -0,0 +1,24 @@
+//document.write("<h1>XDDDDD</h1>");
+// "Hello world" 'Hello world' -- Strings
+
+// 1000  -- Integer 
+// 10.1 -- dec
+// -15 -- negative
+// Boolean data
+//true
+//false
+// array
+/*
+['Luigi', 'Copito', 'Barbitas']
+
+// object
+{
+	"Casa": "Bonita",
+	"Edad": 75,
+	"Score": -25,
+	"Cagado": True,
+}
+*/
+
+var pendejo="Luigi";
+console.log(pendejo);
